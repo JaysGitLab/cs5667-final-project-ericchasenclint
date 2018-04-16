46ef006bc3d3983071c87eca5074914f0de918f1
Eric Cambel <cambelem@appstate.edu>
2018-04-16 15:59:27 +0000

    Fix.

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -19,8 +19,8 @@ const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
  * Webpack Constants
  */
 const ENV = process.env.ENV = process.env.NODE_ENV = 'development';
-const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'cs5667.ml';
+const PORT = process.env.PORT || 3002;
 const HMR = helpers.hasProcessFlag('hot');
 const METADATA = webpackMerge(commonConfig({env: ENV}).metadata, {
   host: HOST,
