a74111845bdb2bc34c5238fc00c86e93e9b8d10b
unknown <bettingerchasen@gmail.com>
2018-04-17 10:27:14 -0400

    Added file strucutre of team-select

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home';
-import { ReactComponent } from './react';
-import { NoContentComponent } from './no-content';
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./home";
+import { ReactComponent } from "./react";
+import { NoContentComponent } from "./no-content";
+import { TeamSelectComponent } from "./team-select";
 
-import { DataResolver } from './app.resolver';
+import { DataResolver } from "./app.resolver";
 
 export const ROUTES: Routes = [
-  { path: '',      component: HomeComponent },
-  { path: 'react', component: ReactComponent },
-  { path: '**',    component: NoContentComponent },
+  { path: "", component: HomeComponent },
+  { path: "react", component: ReactComponent },
+  // { path: "select", component: TeamSelectComponent },
+  { path: "**", component: NoContentComponent }
 ];
---
+++ src/app/team-select/team.select.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "team", // <team></team>
+  styleUrls: ["./team.select.component.scss"],
+  templateUrl: "./team.select.component.html"
+})
+export class TeamSelectComponent {}
