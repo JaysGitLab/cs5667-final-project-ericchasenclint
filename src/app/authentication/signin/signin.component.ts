5fd1c691dd8eacca3806df0c61697d260ec85c5f
Eric Cambel <cambelem@appstate.edu>
2018-04-23 00:35:20 +0000

    Added email checking on front end.

--- src/app/authentication/signin/signin.component.ts
+++ src/app/authentication/signin/signin.component.ts
@@ -17,8 +17,19 @@ export class SigninComponent {
   }
 
   signin() {
+    if(!this.validateEmail(this.credentials.username)){
+        this.errorMessage = "Invalid email, please make sure it's an email.";
+        this.show = true;
+        return
+    }
+      
     this._authenticationService.signin(this.credentials).subscribe(result  => this._router.navigate(['/']), 
                                                                    error =>  this.errorMessage = error );
     this.show = true;
+  }
+  
+  validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
   }
 }
