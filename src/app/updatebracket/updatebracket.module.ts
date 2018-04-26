b03ae0a4666dae29753dac5d004ea308b0b9c417
Clint Hall <bclinthall@gmail.com>
2018-04-26 12:50:46 +0000

    things work again

--- src/app/updatebracket/updatebracket.module.ts
+++ src/app/updatebracket/updatebracket.module.ts
@@ -5,7 +5,6 @@ import { RouterModule } from '@angular/router';
 
 import { UpdateBracketRoutes } from './updatebracket.routes';
 import { UpdateBracketComponent} from './updatebracket.component';
-import { BoxSelectComponent } from './boxselect/boxselect.component';
 import { BracketComponent } from './bracket/bracket.component';
 
 
@@ -23,7 +22,6 @@ import {
     ],
     declarations: [
         UpdateBracketComponent,
-        BoxSelectComponent,
         BracketComponent
     ]
 })
