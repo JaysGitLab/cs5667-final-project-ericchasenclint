1c1c070ef6558fa30a1ba91df46b56064284b4ff
Eric Cambel <cambelem@appstate.edu>
2018-05-03 21:28:02 +0000

    Shows error messages in signup screen. Fixed links.

--- src/app/authentication/signup/signup.component.ts
+++ src/app/authentication/signup/signup.component.ts
@@ -12,6 +12,7 @@ import { LocalStorage } from 'ngx-store';
 export class SignupComponent {
     @LocalStorage('user') user: any;
     errorMessage: string;
+    show: boolean;
     newUser: any = {};
     
     constructor (private _authenticationService:
@@ -20,8 +21,41 @@ export class SignupComponent {
 
 
     signup() {
+        this.validateFields();
+        if (this.show) {
+            return
+        }
+        
         this._authenticationService.signup(this.newUser)
         .subscribe(result => this._router.navigate(['/']),
         error => this.errorMessage = error);
+        
+        this.show = true;
+    }
+    
+    validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+    
+    validateFields() {
+        if(this.newUser.fullName == undefined || this.newUser.username == undefined || this.newUser.password == undefined) {
+            this.errorMessage = "Please fill in the missing fields.";
+            this.show = true;
+            return
+        }
+        
+        if(!this.validateEmail(this.newUser.username)){
+            this.errorMessage = "Invalid email, please make sure it's an email.";
+            this.show = true;
+            return
+        }
+        
+        let pass = this.newUser.password.trim();
+        if(pass == "" || pass == undefined){
+            this.errorMessage = "Please provide a password.";
+            this.show = true;
+            return
+        }   
     }
 }
